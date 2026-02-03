c021663c41e7a237bb29a818f5df3b82482f91ab
ghendisrahmaninggalih <ghendisgalih01@gmail.com>
2026-02-03 11:06:57 +0700

    Update script_fetch.js

--- script_fetch.js
+++ script_fetch.js
@@ -2,7 +2,7 @@ const tbody = document.getElementById("data-siswa");
 
 // mapping ID kelas → nama kelas (contoh)
 const classMap = {
-  "5fd01639-866b-48d0-b4a8-aefb67791964": "XI-C1"
+  "5fd01639-866b-48d0-b4a8-aefb67791964": "XI-C2"
 };
 
 // fungsi bantu: kapitalisasi nama
@@ -13,7 +13,7 @@ function capitalizeName(name) {
     .join(" ");
 }
 
-fetch("students_rows.json")
+fetch("student_rows.json")
   .then(response => {
     if (!response.ok) {
       throw new Error("Gagal mengambil data");
@@ -42,4 +42,5 @@ fetch("students_rows.json")
       </tr>
     `;
   });
+
 
